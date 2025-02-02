970d259dcabc32ce8e21f21d520811bd3358a20a
linkui <123308481+linkui8@users.noreply.github.com>
2025-02-02 17:33:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ from util import Direction
 
 
 
-
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
+    sound_in = pygame.mixer.Sound('data/souds/inmusic.ogg')
+    sound_in.play()
     a = open("level.txt", "r")
     game = Game(a.read())
     a.close()
+    pygame.mixer.music.load("data/souds/background.mp3")
+    pygame.mixer.music.play(-1)
+    pygame.mixer.music.set_volume(0.6)
+    sound_shoot = pygame.mixer.Sound('data/souds/shoot.ogg')
+    sound_move = pygame.mixer.Sound('data/souds/move.ogg')
+
 
     running = True
     while running:
@@ -26,6 +33,9 @@ if __name__ == '__main__':
                     running = False
                 elif event.key == K_SPACE:
                     game.fire()
+                    sound_move.stop()
+                    sound_shoot.play()
+                    sound_move.play()
                 elif event.key == K_r:
                     game = Game()
                 elif event.key == K_p:
@@ -35,14 +45,23 @@ if __name__ == '__main__':
 
         if keys[pygame.K_w]:
             game.my_tank_move_to_direction = Direction.UP
+            sound_move.play()
+            sound_move.set_volume(0.1)
         elif keys[pygame.K_s]:
             game.my_tank_move_to_direction = Direction.DOWN
+            sound_move.play()
+            sound_move.set_volume(0.1)
         elif keys[pygame.K_a]:
             game.my_tank_move_to_direction = Direction.LEFT
+            sound_move.play()
+            sound_move.set_volume(0.1)
         elif keys[pygame.K_d]:
             game.my_tank_move_to_direction = Direction.RIGHT
+            sound_move.play()
+            sound_move.set_volume(0.1)
         else:
             game.my_tank_move_to_direction = None
+            sound_move.stop()
         
         screen.fill((128, 128, 128))
 
